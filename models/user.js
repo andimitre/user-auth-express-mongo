fb68de3509b902c4397c15a73b69dfea81dc5db0
andimitre <23andimitre@gmail.com>
2016-08-08 18:22:57 -0400

    project

---
+++ models/user.js
@@ -0,0 +1,64 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
+var UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  favoriteBook: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true
+  }
+});
+// authenticate input against the db
+UserSchema.statics.authenticate = function(email, password, callback){
+  User.findOne({email: email})
+    .exec(function(error, user){
+    if (error) {
+      return callback(error);
+    } else if (!user) {
+      var err = new Error('User not found');
+      err.status = 401;
+      return callback(err);
+    }
+    bcrypt.compare(password, user.password, function(error, result){
+      if (result === true) {
+        return callback(null, user);
+      } else {
+        return callback();
+      }
+    });
+  });
+};
+
+// hash password before saving to db
+// before saving the object apply the hash
+UserSchema.pre('save', function(next){
+  var user = this;
+  // use bcrypt.hash for hashing
+  bcrypt.hash(user.password, 10, function(err, hash){
+    // supply the users password, how many times to apply the encryption algo, and a callback
+    if (err){
+      return next(err);
+    } else {
+      user.password = hash;
+      next();
+    }
+  })
+});
+
+var User = mongoose.model('User', UserSchema);
+module.exports = User;
---
+++ routes/index.js
@@ -0,0 +1,110 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../models/user');
+var mid = require('../middleware');
+
+// get login
+router.get('/login', mid.loggedOut, function(req, res, next){
+  return res.render('login', { title: 'Log In'});
+});
+
+router.get('/logout', function(req, res, next){
+  if (req.session) {
+    // delete session
+    req.session.destroy(function(err){
+      if (err) {
+        return next(err);
+      } else {
+        return res.redirect('/');
+      }
+    });
+  }
+});
+// post login
+router.post('/login', function(req, res, next){
+  if (req.body.email && req.body.password) {
+    User.authenticate(req.body.email, req.body.password, function(error, user){
+      if (error){
+        var err= new Error('Wrong email or password.');
+        err.status = 401; // authorized
+        return next(err);
+      } else {
+        req.session.userId = user._id;
+        return res.redirect('/profile');
+      }
+    });
+  } else {
+    var err= new Error('All Fields are required.');
+    err.status = 401; // authorized
+    return next(err);
+  }
+});
+
+// GET /
+router.get('/', function(req, res, next) {
+  return res.render('index', { title: 'Home' });
+});
+
+// GET /about
+router.get('/about', mid.requiresLogin, function(req, res, next) {
+  return res.render('about', { title: 'About' });
+});
+
+
+// GET /contact
+router.get('/contact', function(req, res, next) {
+  return res.render('contact', { title: 'Contact' });
+});
+
+// get /register
+router.get('/register', mid.loggedOut, function(req, res, next){
+  return res.render('register', { title: 'Sign Up'});
+});
+
+router.get('/profile', function(req, res, next){
+  if (!req.session.userId) {
+    var err= new Error('No Access.Please login.');
+    err.status = 401;
+    return next(err);
+  } else {
+    User.findById(req.session.userId).exec(function(error, user){
+      return res.render('profile', { title: 'Profile', name: user.name, favorite: user.favoriteBook});
+    });
+  }
+});
+
+// post /register
+router.post('/register', function(req, res, next){
+  if (req.body.email && req.body.name && req.body.favoriteBook
+    && req.body.password && req.body.confirmPassword) {
+    console.log(req.body.email);
+    console.log(req.body.name);
+
+    // create an object with form input
+    // this object represents the doc we want to insert to mongo
+    var userData = {
+      email: req.body.email,
+      name: req.body.name,
+      favoriteBook: req.body.favoriteBook,
+      password: req.body.password
+    };
+
+    // insert doc into mongo
+    User.create(userData, function(error, user){
+      if (error){
+        return next(error);
+      } else {
+        req.session.userId = user._id;
+        return res.redirect('/profile');
+      }
+    });
+
+
+  } else {
+    var err= new Error('All Fields are required.');
+    err.status = 404;
+    return next(err);
+  }
+});
+
+module.exports = router;
